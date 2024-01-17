070306cc30a559db8fc6f7dc7084e6ffcaefdf6f
nate <59595919+natewbrooks@users.noreply.github.com>
2024-01-17 16:04:12 -0500

    Removes supertokens bs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,35 +1,8 @@
 import React from 'react';
 import { LocationProvider } from '@reach/router';
-import SuperTokens, { SuperTokensWrapper } from 'supertokens-auth-react';
-import ThirdPartyEmailPassword, {
-	Google,
-} from 'supertokens-auth-react/recipe/thirdpartyemailpassword';
-import Session from 'supertokens-auth-react/recipe/session';
-
-SuperTokens.init({
-	appInfo: {
-		appName: 'Positive Professionals',
-		apiDomain: 'http://localhost:8000/api/',
-		websiteDomain: 'http://localhost:8000',
-		apiBasePath: '/auth',
-		websiteBasePath: '/login',
-	},
-	recipeList: [
-		ThirdPartyEmailPassword.init({
-			signInAndUpFeature: {
-				providers: [Google.init()],
-			},
-		}),
-		Session.init(),
-	],
-});
 
 export const wrapRootElement = ({ element }) => {
 	console.log(SuperTokensWrapper);
 
-	return (
-		<SuperTokensWrapper>
-			<LocationProvider>{element}</LocationProvider>
-		</SuperTokensWrapper>
-	);
+	return <LocationProvider>{element}</LocationProvider>;
 };
