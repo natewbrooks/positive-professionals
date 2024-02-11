d6f0b596b617f771a00cf0d80f8937b52f2260bf
nate <59595919+natewbrooks@users.noreply.github.com>
2024-02-11 14:22:52 -0500

    Update ResourcesGridLayout.js

--- src/components/resources/ResourcesGridLayout.js
+++ src/components/resources/ResourcesGridLayout.js
@@ -28,7 +28,7 @@ const ResourcesGridLayout = ({ mediaItems }) => {
 		<div className='w-full h-full mb-20'>
 			<div className='w-full h-fit flex null:justify-center sm:justify-end  mb-2 items-center'>
 				<div className='relative -space-x-1 flex w-fit h-fit'>
-					<div className='bg-dark dark:bg-light/70 w-fit h-full flex justify-center items-center py-2 px-3 rounded-l-full'>
+					<div className='bg-dark dark:bg-light/70 w-fit h-full flex justify-center items-center py-3 lg:py-2 px-3 rounded-l-full'>
 						<FaSearch
 							size={14}
 							className='text-light dark:text-darkAccent'
@@ -39,11 +39,11 @@ const ResourcesGridLayout = ({ mediaItems }) => {
 						placeholder='Search keywords...'
 						value={searchTerm}
 						onChange={(e) => setSearchTerm(e.target.value)}
-						className='w-[150px] text-sm placeholder:text-sm xbold sans lg:text-md px-2 border-2 border-l-0 border-darkAccent dark:border-light/70 rounded-r-full outline-none bg-light dark:bg-dark text-dark placeholder:text-dark/50 dark:placeholder:text-light/50 dark:text-light/70'
+						className='w-[180px] text-md placeholder:text-md lg:text-sm lg:placeholder:text-sm xbold sans px-2 border-2 border-l-0 border-darkAccent dark:border-light/70 rounded-r-full outline-none bg-light dark:bg-dark text-dark placeholder:text-dark/50 dark:placeholder:text-light/50 dark:text-light/70'
 					/>
 				</div>
 			</div>
-			<div className='w-full null:text-center sm:text-end text-dark/50 dark:text-light/50 sans xbold text-md pb-2'>
+			<div className='w-full null:text-center sm:text-end text-dark/50 dark:text-light/50 sans xbold text-md pb-8 sm:pb-2'>
 				SHOWING {showingCount} OF {mediaItems.length}
 			</div>
 			<div className='w-full h-full justify-items-center grid null:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xxl:grid-cols-4 null:gap-y-8 sm:gap-y-6 '>
