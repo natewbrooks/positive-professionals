dcc62ee1a699fdaedf02a5225011136f0a1ab5ed
nate <59595919+natewbrooks@users.noreply.github.com>
2024-02-22 11:06:09 -0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,8 +3,6 @@ const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 const { fmImagesToRelative } = require('gatsby-remark-relative-images');
 
-const webpack = require('webpack');
-
 exports.createPages = ({ actions, graphql }) => {
 	const { createPage } = actions;
 
@@ -86,21 +84,28 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 	}
 };
 
-exports.onCreateWebpackConfig = ({ actions, plugins, ...args }) => {
+const webpack = require('webpack'); // Make sure webpack is required
+
+exports.onCreateWebpackConfig = ({ actions, ...args }) => {
 	const buildWebpackConfig =
 		args.stage === 'build-html'
 			? {
 					resolve: {
 						// Handle Uncaught TypeError: util.inherits is not a function - https://github.com/webpack/webpack/issues/1019
 						mainFields: ['browser', 'module', 'main'],
-						// Handle unsupported node scheme - https://github.com/webpack/webpack/issues/13290#issuecomment-987880453
+						// Extend the fallback configuration here
 						fallback: {
 							util: require.resolve('util'),
 							stream: require.resolve('stream-browserify'),
+							// Add the additional fallbacks
+							'stream/web': require.resolve('stream-browserify'),
+							'util/types': require.resolve('util'),
+							diagnostics_channel: require.resolve('diagnostics_channel'),
 						},
 					},
 			  }
 			: {};
+
 	actions.setWebpackConfig({
 		plugins: [
 			// Handle unsupported node scheme - https://github.com/webpack/webpack/issues/13290#issuecomment-987880453
